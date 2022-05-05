4486e839c8a640e1f0d96214444b6f23864006f9
Miqueias <miqueias.sousa.ti@gmail.com>
2022-05-05 19:34:14 -0300

    Alterações e correções menu mobile

--- js/main.js
+++ js/main.js
@@ -1,4 +1,43 @@
 jQuery(document).ready(function ($) {
+  
+  /* sticky navbar on scroll script 
+  $(window).scroll(function(){
+    if(this.scrollY > 20){
+        $('.navbar').addClass("sticky");
+    }else{
+        $('.navbar').removeClass("sticky");
+    }
+    
+    
+    // scroll-up button show/hide script
+    if(this.scrollY > 500){
+        $('.scroll-up-btn').addClass("show");
+    }else{
+        $('.scroll-up-btn').removeClass("show");
+    }
+
+    
+  });
+  */
+ 
+  // slide-up script
+  $('.scroll-up-btn').click(function(){
+    $('html').animate({scrollTop: 0});
+    // removing smooth scroll on slide-up button click
+    $('html').css("scrollBehavior", "auto");
+  });
+
+  $('.navbar .menu li a').click(function(){
+    // applying again smooth scroll on menu items click
+    $('html').css("scrollBehavior", "smooth");
+  });
+
+  // toggle menu/navbar script
+  $('nav .menu-btn').click(function(){
+    $('nav .menu').toggleClass("active");
+    $('nav .menu-btn i').toggleClass("active");
+  });
+  
   //FIXED HEADER
   window.onscroll = function () {
     if (window.pageYOffset > 140) {
