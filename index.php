4cc1aa245bd83db335b8889c8134309e69690a76
Miqueias <miqueias.sousa.ti@gmail.com>
2023-05-24 08:56:35 -0300

    Envio de Site PIB

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
     <title>PIB Potiraguá</title>
 
-    <!-- Required meta tags --->
+    <!-- Required meta tags ---->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
